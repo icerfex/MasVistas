334788a952e039b0b8b3485abc4eda63a8513837
Ariel Alcocer <gasper-015@hotmail.com>
2018-01-08 19:04:08 -0400

    Initial commit

---
+++ app/src/main/java/com/nextsofts/masvistas/MasVistasActivity.java
@@ -0,0 +1,37 @@
+package com.nextsofts.masvistas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MasVistasActivity extends AppCompatActivity {
+    /*las diferentes vistas son objetos*/
+    private EditText entrada;
+    private TextView salida;
+
+    public MasVistasActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*asignacion de objetos de tipo View*/
+        entrada=(EditText) findViewById(R.id.entrada);
+        salida=(TextView) findViewById(R.id.salida);
+    }
+
+    public void sePulsa(View v){
+        Toast.makeText(this,"pulsado",Toast.LENGTH_SHORT).show();
+    }
+    /*metodo escuchador de evento(listener)
+    * toast es un aviso que permanece en la pantalla solo un tiempo y luego desaparece
+    * como parametro del metodo se pasa la vista*/
+    public void sePulsa0(View view)
+    {
+        salida.setText(entrada.getText()+(String)view.getTag());
+    }
+}
