55d959f94c01237dc66df5516db1eea22142e145
icerfex <icerfex@gmail.com>
2018-01-08 22:30:06 -0400

    version 7

--- app/src/main/java/com/nextsofts/masvistas/MasVistasActivity.java
+++ app/src/main/java/com/nextsofts/masvistas/MasVistasActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+/*hola prueba de cvssistema de control de versiones*/
 public class MasVistasActivity extends AppCompatActivity {
 
     private EditText entrada;
